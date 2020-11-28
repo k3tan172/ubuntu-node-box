fe4721b124c339ca783de128b9fc716b5f02bd51
k3tan172 <44537874+k3tan172@users.noreply.github.com>
2020-11-28 22:04:43 +1100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sudo sysctl -w fs.pipe-max-size=33554432
-echo Password1
+sudo echo 33554432 > /proc/sys/fs/pipe-max-size
 /usr/local/bin/blocksat-cli sdr --gui -g 20 --derotate -100 -d
